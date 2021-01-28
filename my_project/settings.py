28948063cb3fb00adfaf3114576cad6f469433da
thinkpad <erlan.kubanychbekov.000@gmail.com>
2021-01-28 22:02:14 +0600

    ...

--- my_project/settings.py
+++ my_project/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 import dj_database_url
 
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
